d5586657fe451fb101f1ceb521846ed48a6110f7
Kushal Delhiwala <kushaldelhiwala@hotmail.com>
2019-09-15 13:14:29 -0400

    Boilerplate

---
+++ src/main/java/com/cse308/stratego/demo/Cse308AquamarineApplication.java
@@ -0,0 +1,13 @@
+package com.cse308.stratego.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cse308AquamarineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cse308AquamarineApplication.class, args);
+    }
+
+}
