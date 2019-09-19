78cbd8fd2228b5a39750f320e5dd2b75898f03e3
Neil Opena <nopena@cs.stonybrook.edu>
2019-09-18 21:10:29 -0400

    Refined computer move choice algorithm

--- temp-game/temp-stratego/src/helper.js
+++ temp-game/temp-stratego/src/helper.js
@@ -14,19 +14,76 @@ export function getMoveablePieces(game) {
     // for now this function just adds the empty spots as the possible places the pieces can go
     // also all the enemy pieces can move at this point of implementation
 
+    /**
+     * game array marks player positions as 'P'
+     * and computer positions as 'C'
+     * the cells are from cell 0 to cell 99 
+     */
+
     const map = [];
-    let empty_spots = [];
-    let computer_locations = [];
-    for (var i = 0; i < 100; i++) {
-        if (!game[i]) {
-            empty_spots.push(i);
-        } else if (game[i] === 'C') {
-            computer_locations.push(i);
+
+    //first find enemy positions
+    //for each of those positions, check if the piece can go left, right, up, or down
+    //implement piece checking later
+    let computer_squares = [];
+    let i;
+    for (i = 0; i < 100; i++) {
+        if (game[i] === 'C') {
+            computer_squares.push(i);
         }
     }
-    for (var j = 0; j < computer_locations.length; j++) {
-        map.push([computer_locations[j], empty_spots]);
+    for (i = 0; i < computer_squares.length; i++) {
+        let temp = [];
+        let index = computer_squares[i];
+        if (isLeftValid(index, game)) {
+            temp.push(index - 1);
+        }
+        if (isRightValid(index, game)) {
+            temp.push(index + 1);
+        }
+        if (isTopValid(index, game)) {
+            console.log('shits valid apparently');
+            temp.push(index - 10);
+        }
+        if (isBottomValid(index, game)) {
+            temp.push(index + 10);
+        }
+        if (temp.length) {
+            map.push([index, temp])
+        }
     }
 
     return map;
+}
+
+function isLeftValid(i, game) {
+    if (i % 10) {
+        return false;  //current piece is on the left boundary
+    }
+    let left = i - 1;
+    return game[left] === 'C' ? false : true;
+}
+
+function isRightValid(i, game) {
+    if ((i + 1) % 10) {
+        return false; // current piece is on the right boundary
+    }
+    let right = i + 1;
+    return game[right] === 'C' ? false : true;
+}
+
+function isTopValid(i, game) {
+    if (i < 10) {
+        return false; // current piece is on the top boundary
+    }
+    let top = i - 10;
+    return game[top] === 'C' ? false : true;
+}
+
+function isBottomValid(i, game) {
+    if (i >= 90) {
+        return false // current piece is on the bottom boundary
+    }
+    let bottom = i + 10;
+    return game[bottom] === 'C' ? false : true;
 }
