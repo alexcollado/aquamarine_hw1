9f1c8c34e7e0b0748ec1f119b8669f8bb1244ced
Alexander Collado <alexc1117@gmail.com>
2019-09-22 14:26:38 -0400

    Added CSS Modules and React Router. Modified existing CSS as modules. Created login and error pages

---
+++ frontend/src/pages/NewAccount.jsx
@@ -0,0 +1,13 @@
+import React, {Fragment, Component} from 'react';
+
+class NewAccount extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div>New Account</div>
+            </Fragment>
+        );
+    }
+}
+
+export default NewAccount;
